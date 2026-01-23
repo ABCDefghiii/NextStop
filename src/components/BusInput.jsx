03166470c99e8b82e9299a45ec3ff3f1fb00b56a
Shruthi Haasini <shruthihaasinikarri22@gmail.com>
2026-01-23 11:54:00 +0530

    Create BusInput.jsx

---
+++ src/components/BusInput.jsx
@@ -0,0 +1,11 @@
+function BusInput() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter Bus Number" />
+      <input type="text" placeholder="Enter Stop Name" />
+      <button>Get ETA</button>
+    </div>
+  );
+}
+
+export default BusInput;
