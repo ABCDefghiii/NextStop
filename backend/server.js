ece7f407736c94e390404c6d494c97697da6d6d0
Shruthi Haasini <shruthihaasinikarri22@example.com>
2026-04-16 01:53:39 +0530

    Add Vercel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
     origin: [
         "https://next-stop-pi.vercel.app",
-        "http://localhost:3000"
+        ["http://localhost:3000", "https://nextstop-xxxx.vercel.app"]
     ],
     methods: ["GET", "POST"],
     credentials: true
